3b0fe041b51adc83a0710ac47893b5196e55c4ef
jasonldoyle <jliudoyle01@gmail.com>
2025-12-28 22:22:27 +0000

    Favourites added routes setup

---
+++ src/app/favourites/favourites.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonButtons, IonBackButton, IonButton } from '@ionic/angular/standalone';
+
+@Component({
+  selector: 'app-favourites',
+  templateUrl: './favourites.page.html',
+  styleUrls: ['./favourites.page.scss'],
+  standalone: true,
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonButtons, IonBackButton, IonButton]
+
+})
+export class FavouritesPage implements OnInit {
+  favourites: any[] = [];
+  constructor() { }
+
+  ngOnInit() {
+    // TODO: Load favourite from storage
+  }
+
+}
